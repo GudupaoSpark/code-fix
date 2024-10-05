2c43d45202b7c9db830ee75104e0c4b7e5f5c718
Debug967 <89326292+lidongxun967@users.noreply.github.com>
2024-10-05 18:26:46 +0800

    Update copy.js

--- docs/js/copy.js
+++ docs/js/copy.js
@@ -1,3 +1,18 @@
+function toast(text, time) {
+    let toast = document.getElementById('toast');
+    let toast_box = document.getElementsByClassName('toast_box')[0];
+    toast.innerHTML = text;
+    toast_box.style.animation = 'show 1.5s'
+    toast_box.style.display = 'inline-block';
+	setTimeout(function(){
+		toast_box.style.animation = 'hide 1.5s'
+		setTimeout(function(){
+			toast_box.style.display = 'none';
+		}, 1400)
+	}, time)   
+}
+
+
 function copyToClip(content, message) {
     var aux = document.createElement("input"); 
     aux.setAttribute("value", content); 
@@ -6,8 +21,8 @@ function copyToClip(content, message) {
     document.execCommand("copy"); 
     document.body.removeChild(aux);
     if (message == null) {
-        alert("复制成功");
+        toast("复制成功"，3000);
     } else{
-        alert(message);
+        toast(message，3000);
     }
 }
